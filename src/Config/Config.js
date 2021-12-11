9d9626a99a2c6d0a9d9eace73c0fac14d5474c32
Farjana <farjanasultana2612@gmail.com>
2021-12-11 10:59:16 +0600

    Intial Commit

---
+++ src/Config/Config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBoUyKr7GEPfNLhDVdT0gejM8o5vlD5liY",
+    authDomain: "ecom-ef36a.firebaseapp.com",
+    projectId: "ecom-ef36a",
+    storageBucket: "ecom-ef36a.appspot.com",
+    messagingSenderId: "294964433993",
+    appId: "1:294964433993:web:0579cd17f74777d6dd1b98",
+    measurementId: "${config.measurementId}"
+  };
+
+  firebaseConfig.initializea
