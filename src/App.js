1f4b54df8aeadafcf1942cfb72d8ca6679fd02bc
Farjana <farjanasultana2612@gmail.com>
2021-12-11 11:15:54 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './Components/Home'
+import { Login } from './Components/Login'
+import { Signup } from './Components/Signup'
+import { NotFound } from './Components/NotFound'
+import { AddProducts } from './Components/AddProducts'
+import { Cart } from './Components/Cart'
 
-function App() {
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element = {<Home />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/add-products" element={<AddProducts />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
+
 const firebaseConfig = {
     apiKey: "AIzaSyBoUyKr7GEPfNLhDVdT0gejM8o5vlD5liY",
     authDomain: "ecom-ef36a.firebaseapp.com",
@@ -7,5 +12,12 @@ const firebaseConfig = {
     appId: "1:294964433993:web:0579cd17f74777d6dd1b98",
     measurementId: "${config.measurementId}"
   };
+  
 
-  firebaseConfig.initializea
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const fs = firebase.firestore();
+const storage = firebase.storage();
+
+export {auth,fs,storage}
